374307431d9caee617b1656459000f38c46f232c
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-07-25 11:44:38 -0300

    Usando Optional no método buscarProduto no client de Catálogo de Produtos

--- src/main/java/br/com/zup/edu/commerce/marketplace/clients/CatalogoProdutosClient.java
+++ src/main/java/br/com/zup/edu/commerce/marketplace/clients/CatalogoProdutosClient.java
@@ -4,6 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Optional;
+
 @FeignClient(
         name = "catalogoProdutosClient",
         url = "${integrations.clients.catalogo-produtos.url}"
@@ -11,6 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface CatalogoProdutosClient {
 
     @GetMapping("/produtos/{id}")
-    public ProdutoResponse buscarProduto(@PathVariable Long id);
+    public Optional<ProdutoResponse> buscarProduto(@PathVariable Long id);
 
 }
