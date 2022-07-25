6a165ec265d723c3fa1963323061c69099425f39
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-07-25 14:01:20 -0300

    Adicionando validação para a request de pagamento

--- src/main/java/br/com/zup/edu/commerce/marketplace/clients/PagamentoRequest.java
+++ src/main/java/br/com/zup/edu/commerce/marketplace/clients/PagamentoRequest.java
@@ -1,14 +1,28 @@
 package br.com.zup.edu.commerce.marketplace.clients;
 
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.YearMonth;
 
 public class PagamentoRequest {
 
+    @NotBlank
     private String titular;
+
+    @NotBlank
+    @Pattern(regexp = "^\\d{16}$")
     private String numero;
+
+    @NotNull
+    @FutureOrPresent
     private YearMonth validoAte;
+
+    @NotBlank
+    @Pattern(regexp = "^\\d{3}$")
     private String codigoSeguranca;
+
+    @NotNull
+    @Positive
     private BigDecimal valorCompra;
 
     public PagamentoRequest() {
--- src/main/java/br/com/zup/edu/commerce/marketplace/clients/ServicoPagamentosClient.java
+++ src/main/java/br/com/zup/edu/commerce/marketplace/clients/ServicoPagamentosClient.java
@@ -4,6 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.Valid;
+
 @FeignClient(
         name = "servicoPagamentosClient",
         url = "${integrations.clients.servico-pagamentos.url}"
@@ -11,6 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface ServicoPagamentosClient {
 
     @PostMapping("/pagamentos/credito")
-    public PagamentoResponse submeterPagamento(@RequestBody PagamentoRequest request);
+    public PagamentoResponse submeterPagamento(@RequestBody @Valid PagamentoRequest request);
 
 }
