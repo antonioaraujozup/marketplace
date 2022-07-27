a39121550eb10682c28f4624d45aefe859047928
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-07-27 08:11:19 -0300

    Criando repositório ItemVendaRepository

---
+++ src/main/java/br/com/zup/edu/commerce/marketplace/compra/ItemVendaRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.edu.commerce.marketplace.compra;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemVendaRepository extends JpaRepository<ItemVenda,Long> {
+}
