7183e1a13421546a77e04141b13842d68d3e5f2c
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-07-27 15:40:50 -0300

    Alterando a estratégia de geração de ID para a entidade ItemVenda

--- src/main/java/br/com/zup/edu/commerce/marketplace/compra/ItemVenda.java
+++ src/main/java/br/com/zup/edu/commerce/marketplace/compra/ItemVenda.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 public class ItemVenda {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
